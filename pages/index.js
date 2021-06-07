08af489b60899e6971f73474033d2a0274f838ab
Oskar Przyborski <oskar.przski@gmail.com>
2021-06-07 21:46:46 +0200

    added css, more data shown

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,62 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Styles from '../Styles/Home.module.css'
 
 export default function Home() {
+  function shakeHorizontal(element,maxX,shakeTimes) {
+    const prevColor = element.style.backgroundColor
+    element.style.backgroundColor = "#dc3545"
+    element.style.borderColor = "#dc3545"
+    let counter=1
+    let nextXmove = maxX
+    let slowUnit = maxX / shakeTimes
+    let reverse = 1
+    const shake = () => {
+      element.style.transform = `translate(${nextXmove*reverse}px,0px)`;
+      if(reverse==1)reverse=-1
+      else reverse = 1
+      nextXmove -= slowUnit
+      console.log(nextXmove*reverse)
+      setTimeout(()=>{
+        counter += 1
+        if(counter <= shakeTimes){
+          shake()
+        }else{
+          element.style.transform = 'translate(0px,0px)';
+          element.style.backgroundColor = prevColor
+          element.style.borderColor = prevColor
+        }
+      },100)
+    }
+    shake()
+}
+
   const search = () => {
-    window.open(`${window.location.href}weather/${document.getElementById("CityName").value}`,"_self")
+    const alertSpan = document.getElementById("alertSpan")
+    let inputVal = document.getElementById("CityName").value
+    if(inputVal=="") shakeHorizontal(document.getElementById("CityName"),30,4)
+    else window.open(`${window.location.href}weather/${inputVal}`,"_self")
+    
+  }
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      search()
+    }
   }
   return (
-    <div className={styles.container}>
+    <div className={Styles.container}>
       <Head>
         <title>WeatherFilter</title>
         <meta name="description" content="Find day by weather with WeatherFilter!" />
       </Head>
-      <main className={styles.main}>
-        City <input id="CityName" type="text"/>
-        <br/>
-        <a onClick={search}>Search</a>
-      </main>
+      <div className={"my-5"}>
+        <h1>Welcome in WeatherFilter!</h1>
+        <h3>Find day by weather!</h3>
+      </div>
+      <div className={"my-5"}>
+        <input className={Styles.inputBox}id="CityName" type="text" onKeyDown={handleKeyDown} placeholder="Localization"/><br/>
+        <input type="button" onClick={search} className={Styles.searchBtn} value="Get weather!"/><br/>
+        <span id="alertSpan"></span>
+      </div>
     </div>
   )
 }
--- pages/weather/[city_name].js
+++ pages/weather/[city_name].js
@@ -1,23 +1,37 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Styles from '../../styles/Result.module.css'
 
-export default function WeatherSearch({dataWeather}) {
+export default function WeatherSearch({dataWeather,latLonData}) {
+
   const getResult = () => {
-    if(dataWeather === null) return("city not found")
-    else return(`Now it\'s: ${dataWeather.current.weather[0].main}`)
+    if(dataWeather === null) return(<>
+        Address not found :( <br/>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </>
+    )
+    else return(
+      <>
+        <h3>Localization: {latLonData[0].name}</h3>
+        <h5>Weather - {dataWeather.current.weather[0].description} <img className="img-fluid" width="30px" src={`http://openweathermap.org/img/wn/${dataWeather.current.weather[0].icon}.png`}/></h5>
+        <h5>Temperature - {dataWeather.current.temp}°C</h5>
+        <h5>Perceptible temperature - {dataWeather.current.feels_like}°C</h5>
+        <h5>Wind speed - {dataWeather.current.wind_speed} m/s</h5>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </>
+    )
   }
   return (
-    <div>
+    <div className={Styles.container}>
       <Head>
         <title>WeatherFilter</title>
         <meta name="description" content="Find day by weather with WeatherFilter!" />
       </Head>
-      <main>
-        {getResult()} <br/>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </main>
+      {getResult()}
     </div>
   )
 }
@@ -26,12 +40,12 @@ export async function getServerSideProps(context) {
   let dataWeather = null
   let latLonData = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${context.query.city_name.normalize("NFD").replace(/[\u0300-\u036f]/g, "")}&limit=1&appid=187db54233cfd9ab55e609decd15cc0d`)
   latLonData = await latLonData.json()
-  if(latLonData[0] == null) return {props: {dataWeather}} 
+  if(latLonData[0] == null) return {props: {dataWeather,latLonData}} 
   let lat = latLonData[0].lat
   let lon = latLonData[0].lon
   dataWeather = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=187db54233cfd9ab55e609decd15cc0d`)
   dataWeather = await dataWeather.json()
   return {
-    props: {dataWeather}
+    props: {dataWeather,latLonData}
   }
 }
